e434f338ecb161ece5693e92a08b2d07ecfa4a17
jaimegodoyt-prog <jaimegodoy.t@gmail.com>
2026-08-12 11:34:04 -0400

    Updated AditivosCierre.tsx

--- src/components/AditivosCierre.tsx
+++ src/components/AditivosCierre.tsx
@@ -256,7 +256,7 @@ export function AditivosCierre({
                 </tr>
               </thead>
               <tbody>
-                {turno.otrosInsumos.map((o) => (
+                {(turno.otrosInsumos ?? []).map((o) => (
                   <tr key={o.id} className="border-b border-slate-700/40">
                     <td className="px-3 py-2">
                       <input

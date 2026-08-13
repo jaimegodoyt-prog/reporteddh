8478f9b77ec2483e5dd65055269041cedc61fb87
jaimegodoyt-prog <jaimegodoy.t@gmail.com>
2026-08-12 20:54:39 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1031,21 +1031,12 @@ export default function App() {
           )}
           {pestaña === "cierre" && (
             esUUIDValido(turno.cloudId) ? (
-             <AditivosCierre
+              <AditivosCierre
                 turno={turno}
                 onAgregarInsumo={agregarInsumo}
                 onChangeInsumo={cambiarInsumo}
                 onBlurInsumos={syncInsumosOnBlur}
                 onEliminarInsumo={eliminarInsumo}
-                onChangeDiesel={cambiarDiesel}
-                onChangeHorometroFinal={cambiarHorometroFinal}
-                onAgregarOtroInsumo={agregarOtroInsumo}
-                onChangeOtroInsumo={cambiarOtroInsumo}
-                onBlurOtrosInsumos={syncOtrosInsumosOnBlur}
-                onEliminarOtroInsumo={eliminarOtroInsumo}
-                onAgregarHerramienta={agregarHerramienta}
-                onChangeHerramienta={cambiarHerramienta}
-                onBlurHerramienta={syncHerramientaOnBlur}
                 onChangeObservaciones={(v) => patchTurno({ observaciones: v })}
                 onFirmaChange={(v) => patchTurno({ firmaDataURL: v })}
                 onCerrarTurno={cerrarTurno}
